292610bf9c53d4daa05a7eb6fc21d995b2098915
rabie02 <routkidout@gmail.com>
2024-10-25 23:23:47 +0100

    Tp2myHrOutkidoutRabieAamaraAchrafApplication

---
+++ src/main/java/com/myhr/api_sb/controller/EmployeeController.java
@@ -0,0 +1,21 @@
+package com.myhr.api_sb.controller;
+
+import com.myhr.api_sb.model.Employee;
+import com.myhr.api_sb.service.EmployeeService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class EmployeeController {
+    @Autowired
+    private EmployeeService employeeService;
+    /**
+     * Read - Get all employees
+     * @return - An Iterable object of Employee full filled
+     */
+    @GetMapping("/employees")
+    public Iterable<Employee> getEmployees() {
+        return employeeService.getEmployees();
+    }
+}
---
+++ src/main/java/com/myhr/api_sb/model/Employee.java
@@ -0,0 +1,19 @@
+package com.myhr.api_sb.model;
+
+import jakarta.persistence.*;
+import lombok.Data;
+
+@Data
+@Entity
+@Table(name = "employees")
+public class Employee {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @Column(name="first_name")
+    private String firstName;
+    @Column(name="last_name")
+    private String lastName;
+    private String mail;
+    private String password;
+}
